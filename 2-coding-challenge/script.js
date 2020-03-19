e1499bcf3874e68861ded2a1edadbf1ca9b87754
Sergio Flores Ledezma <zerchito@gmail.com>
2020-03-18 23:40:58 -0400

    second challenge

---
+++ 2-coding-challenge/script.js
@@ -0,0 +1,36 @@
+let jhon = (89 + 120 + 103)/3;
+let mike = (116 + 94 + 123)/3;
+console.log(jhon,mike)
+if(jhon > mike) {
+  console.log('Jhon won:', jhon);
+} else {
+  console.log('Mike won:', mike);
+}
+
+jhon = (150 + 120 + 103)/3;
+
+if(jhon > mike) {
+  console.log('Jhon won:', jhon);
+} else {
+  console.log('Mike won:', mike);
+}
+
+let maria = (97 + 134 + 105)/3
+
+if (maria > jhon && maria > mike) {
+  console.log('Maria won:', maria);
+} else if (jhon > maria && jhon > mike){
+  console.log('jhon won:', jhon);
+} else {
+  console.log('Mike won:', mike);
+}
+
+mike = (116 + 194 + 123)/3;
+
+if (maria > jhon && maria > mike) {
+  console.log('Maria won:', maria);
+} else if (jhon > maria && jhon > mike){
+  console.log('jhon won:', jhon);
+} else {
+  console.log('Mike won:', mike);
+}
